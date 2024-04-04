0fcf1ce498e7975e0933a500cec697533e9e2e39
Ivan Suvorin <ivan.suv99@gmail.com>
2024-04-04 15:47:37 +0300

    added config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,19 @@
+import { BuildOptions } from './types/config';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+
+
+export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
+
+   return [
+      new HTMLWebpackPlugin({
+         template: options.paths.html,
+      }),
+      new webpack.ProgressPlugin(),
+      new MiniCssExtractPlugin({
+         filename: 'css/[name].[contenthash:8].css',
+         chunkFilename: 'css/[name].[contenthash:8].css',
+      })
+   ]
+}
